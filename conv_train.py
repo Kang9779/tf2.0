01ceca7b00c384fada166bce8552806d4d7a3379
kangzhang <kangzhang_seu@163.com>
2019-11-25 09:25:56 +0800

    model file

---
+++ conv_train.py
@@ -0,0 +1,71 @@
+import os
+import time
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets,layers,models,optimizers,metrics
+from tensorflow.python.ops import summary_ops_v2
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def mnist_datasets():
+    (x_train,y_train),(x_test,y_test) = datasets.mnist.load_data()
+    x_train,x_test = x_train/np.float32(255),x_test/np.float32(255)
+    y_train,y_test = y_train.astype(np.int64),y_test.astype(np.int64)
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train,y_train))
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test,y_test))
+    return train_dataset,test_dataset
+
+train_ds,test_ds = mnist_datasets()
+train_ds = train_ds.shuffle(60000).batch(100)
+test_ds = test_ds.batch(100)
+
+model = tf.kears.Sequential([
+    layers.Reshape(
+        target_shape=[28,28,1],
+        input_shape = (28,28,)
+    ),
+    layers.Conv2D(2,5,padding='same',activation=tf.nn.relu),
+    layers.MaxPolling2D((2,2),(2,2),padding='same'),
+    layers.Conv2D(4,5,padding='same',activation=tf.nn.relu),
+    layers.MaxPolling2D((2,2),(2,2),padding='same'),
+    layers.Flatten(),
+    layers.Dense(32,activation=tf.nn.relu),
+    layers.Dropout(rate=0.4),
+    layers.Dense(10)]
+)
+optimizer = optimizers.SGD(learing_rate=0.01,momentum=0.5)
+compute_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+compute_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+
+def train_step(nodel,optimizer,images,labels):
+    with tf.GradientTape() as tape:
+        logits = model(images,training=True)
+        loss = compute_loss(labels,logits)
+        compute_accuracy(labels,logits)
+    grads = tape.gradient(loss,model.trainable_variables)
+    optimizer.apply_gradients(zip(grads,model.trainable_variables))
+    return loss
+
+def train(model,optimizer,dataset,log_freq=50):
+    avg_loss = metrics.Mean('loss',dtype=tf.float32)
+    
+    for images,labels in dataset:
+        loss = train_step(model,optimizer,images,labels)
+        avg_loss(loss)
+
+        if tf.equal(optimizer.iterations % log_freq,0):
+            print('step',int(optimizer.iterations),
+                'loss',avg_loss.result().numpy(),
+                'acc',compute_accuracy.result().numpy()
+            )
+            avg_loss.reset_states()
+            compute_accuracy.reset_states()
+
+def test(model,dataset,step_num):
+    avg_loss = metrics.Mean('loss',dtype=tf.float32)
+    for (images,labels) in dataset:
+        logits = model(images,training=False)
+        avg_loss(compute_loss(labels,logits))
+        compute_accuracy(labels,logits)
+        
